f53ae4e439818ab4cb32d3a067ba19572a9ccb45
max-jellycat <max@jellycat.fr>
2021-02-24 23:01:11 +0100

    api: GET & POST route

--- api/src/controllers/main.controller.ts
+++ api/src/controllers/main.controller.ts
@@ -11,5 +11,7 @@ export class Controller {
 
   public routes() {
     this.app.route('/').get(this.mainService.welcomeMessage);
+    this.app.route('/pokemons').get(this.mainService.getAllPokemon);
+    this.app.route('/pokemon').post(this.mainService.addNewPokemon);
   }
 }
--- api/src/services/main.service.ts
+++ api/src/services/main.service.ts
@@ -1,7 +1,46 @@
 import { Request, Response } from 'express';
+import { MongooseDocument } from 'mongoose';
+
+import { Pokemon } from '../models/pokemon.model';
 
 export class MainService {
   public welcomeMessage(req: Request, res: Response) {
-    return res.status(200).send("Welcome to the sample API 👋");
+    return res.status(200).send('Welcome to the sample API 👋');
+  }
+
+  public async getAllPokemon(req: Request, res: Response) {
+    try {
+      const pokemon = await Pokemon.find();
+      res
+        .status(200)
+        .json(pokemon);
+      console.log('✅ FETCHED POKEMONS');
+    } catch(err) {
+      console.log('❌ FAILED TO FETCH POKEMONS');
+      console.error(err.message);
+      res
+        .status(500)
+        .json({ message: 'Failed to fetch pokemons' });
+    }
+  }
+
+  public async addNewPokemon(req: Request, res :Response) {
+    const newPokemon = new Pokemon(req.body);
+
+    try {
+      const pokemon = await newPokemon.save();
+      if (pokemon) {
+        res
+          .status(201)
+          .json({ message: 'Pokemon saved', pokemon });
+        console.log('✅ STORED NEW POKEMON');
+      }
+    } catch(err) {
+      console.log('❌ ERROR SAVING POKEMON');
+      console.error(err.message);
+      res
+        .status(500)
+        .json({ message: 'Failed to save pokemon' })
+    }
   }
 }
